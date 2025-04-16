1764eab68953ef278bdade53273da598bfab4bb5
Tam107 <oicai107@gmail.com>
2025-04-16 22:20:38 +0700

    add JWT Service for refresh and expiration token

--- src/main/java/org/ecommercecv/service/JwtService.java
+++ src/main/java/org/ecommercecv/service/JwtService.java
@@ -1,7 +1,107 @@
 package org.ecommercecv.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import io.jsonwebtoken.io.Decoders;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 @Service
+@Slf4j
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    @Value("${jwt.refresh-expiration}")
+    private Long refreshExpiration;
+
+
+    /**
+     * what we need to implement
+     * step 1: create a method to generate token
+     * step 2: create a method to extract username from token
+     * step 3: create a method to extract expiration from token
+     * step 4: create a method to extract claims from token
+     * step 5: create a method to check if token is expired
+     * step 6: create a method to check if token is valid
+     * step 7: create a method to generate refresh token
+     *
+     * */
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return generateToken(claims, userDetails, expiration);
+    }
+
+    // Map<String, Object> claims: Thông tin bổ sung (e.g., roles, email).
+    public String generateToken(Map<String, Object> claims, UserDetails userDetails, long expiration) {
+        return createToken(claims, userDetails.getUsername(), expiration);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject, long expiration) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String generateRefreshToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return generateToken(claims, userDetails, refreshExpiration);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSignInKey())
+                .parseClaimsJws(token).getBody();
+    }
+
+    private Key getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
 }
